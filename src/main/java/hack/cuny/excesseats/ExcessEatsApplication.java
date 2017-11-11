e53dcf3b82b20d62b8b82ee643bb034defee1249
matt-slater <matt.a.slater@gmail.com>
2017-11-11 10:34:50 -0500

    initial commit

---
+++ src/main/java/hack/cuny/excesseats/ExcessEatsApplication.java
@@ -0,0 +1,12 @@
+package hack.cuny.excesseats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcessEatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcessEatsApplication.class, args);
+	}
+}
---
+++ src/test/java/hack/cuny/excesseats/ExcessEatsApplicationTests.java
@@ -0,0 +1,16 @@
+package hack.cuny.excesseats;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExcessEatsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
